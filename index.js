d7aef10aa4995a6ebfd2550416df3952cdb2e1be
Abu Saeid <abusaeid95@gmail.com>
2021-10-31 23:20:26 +0600

    edit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 const app = express()
 app.use(cors())
 app.use(express.json())
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 
 const uri = `mongodb+srv://${process.env.USER_ID}:${process.env.USER_PASS}@cluster0.4ye73.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
